c1d66b3756e3d80547d6a533d6db520bd8fa58fa
Goose <josephtrop@gmail.com>
2017-05-24 19:57:12 -0700

    copied from pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,25 @@
 $(document).ready(function(){
 
 
-    $(".yelp").on("click",function(){
-        var data = {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.yelp.com/oauth2/token",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache",
+            "postman-token": "9cd9b19a-52a7-de96-69e9-0cf241410c9c"
+        },
+        "data": {
             "client_id": "z2UQxENYU2tfHC8qDBhNFg",
             "client_secret": "pkvMp9YG30GnouqQEspkge3YKp1h5f05ZnzzQGA35ImfV7reXfQy0qEt0fNIPS7P"
         }
+    }
 
-        $.ajax({
-            type: "POST",
-            beforeSend: function(request) {
-                request.setRequestHeader("Access-Control-Allow-Origin", "*");
-            },
-            url: "https://api.yelp.com/oauth2/token",
-            processData: false,
-            contentType: 'application/x-www-form-urlencoded',
-            data: JSON.stringify(data),
-            success: function(r) {
-                console.log(r)
-                alert(r)
-            }
-
-            });
-    })
-
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 
 
 
