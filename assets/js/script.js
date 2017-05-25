22f5a0daaed163d8c54cefffb3ec6940e9e77434
Goose <josephtrop@gmail.com>
2017-05-24 19:03:48 -0700

    test

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+    $(".yelp").on("click",function(){
+        var data = { "user" : "me!" };
+        $.ajax({
+            type: "POST",
+            url: "https://api.yelp.com/oauth2/token",
+            dataType:"",
+            processData: false,
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(r) {
+                console.log(r)
+            }
+
+            });
+    })
+
+
+
+
+});
