127f0fbfd340df5f71b8c40f6138d91827d57eef
Goose <josephtrop@gmail.com>
2017-05-24 19:43:05 -0700

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@ $(document).ready(function(){
 
 
     $(".yelp").on("click",function(){
-        var data = { "user" : "me!" };
+        var data = {
+            "client_id": "z2UQxENYU2tfHC8qDBhNFg",
+            "client_secret": "pkvMp9YG30GnouqQEspkge3YKp1h5f05ZnzzQGA35ImfV7reXfQy0qEt0fNIPS7P"
+        }
+
         $.ajax({
             type: "POST",
             url: "https://api.yelp.com/oauth2/token",
             dataType:"",
             processData: false,
-            contentType: 'application/json',
+            contentType: 'application/x-www-form-urlencoded',
             data: JSON.stringify(data),
             success: function(r) {
                 console.log(r)
