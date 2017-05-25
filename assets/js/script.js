f4bfab6523f22c74a7869d48615fe32736e7c486
Shirlon Banks <sssbanks@gmail.com>
2017-05-24 20:58:02 -0700

    zomato update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
 
 
-    $(".yelp").on("click",function(){
-        var data = { "user" : "me!" };
-        $.ajax({
-            type: "POST",
-            url: "https://api.yelp.com/oauth2/token",
-            dataType:"",
-            processData: false,
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            success: function(r) {
-                console.log(r)
-                alert(r)
-            }
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://developers.zomato.com/api/v2.1/categories",
+        "method": "GET",
+        "headers": {
+            "Accept": "application/json",
+            "user-key": "2764611985fca4aa535b451992f20776"
+        },
+        
+     
+    }
 
-            });
-    })
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    
 
 
 
