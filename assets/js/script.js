c6f148516379b9834c8b3b6b8364e0aed2644291
Kevin Brannan <brannake@gmail.com>
2017-05-27 11:16:43 -0700

    Gmaps API

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var map;
+var response1;
+var location;
+var userPosition = {
+  lat: 0,
+  lng: 0
+}
+  //Grabs the search results from Google API
+  function queryGMapsAPI () {
+    $.ajax({
+      url: "http://crossorigin.me/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.912618099999996,-117.1397224&radius=3500&type=restaurant&keyword=food&key=AIzaSyBBojALvC0zFv82BS2A16rIxnq_bBCl4pQ",
+      method: "GET"
+      }).done(function(response) {
+      console.log(response);
+      // Loops through the results array and places a marker for each
+      for (var i = 0; i < 10; i++) {
+        console.log(response);
+        console.log(response.results[i].geometry.location);
+        var coords = response.results[i].geometry.location;
+        var latLng = new google.maps.LatLng(coords.lat,coords.lng);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+          });
+        }
+      }
+    )}
+
+  function initMap() {
+  //Initialize the map and set center and zoom
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: new google.maps.LatLng(32.912618099999996,-117.1397224),
+    mapTypeId: 'terrain'
+    });
+  }
+
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } 
+    else { 
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function showPosition(position) {
+    userPosition.lat = position.coords.latitude;
+    userPosition.lng = position.coords.longitude;
+    console.log(userPosition);
+  }
+
+getLocation();
+initMap();
+queryGMapsAPI();
