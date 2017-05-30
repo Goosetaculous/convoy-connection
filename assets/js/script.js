37dadbfaddc1a64ead09b141564f04b0c3c46f17
Goose <josephtrop@gmail.com>
2017-05-30 13:24:54 -0700

    new branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 3); // Move next n times.
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 4); // Move prev n times.
+    $('.carousel').carousel('set', 4);
 
 
 });
