b757bc1a61090cd1eaedd2a319a4d1c637dc7c40
Goose <josephtrop@gmail.com>
2017-05-30 17:55:29 -0700

    test

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -15,12 +15,12 @@ function queryGMapsAPI (searchterm) {
         url: "http://crossorigin.me/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+userPosition.lat+","+userPosition.lng+"&radius=3500&type=restaurant&keyword="+searchterm+"&key=AIzaSyBBojALvC0zFv82BS2A16rIxnq_bBCl4pQ",
         method: "GET"
     }).done(function(response) {
-            console.log(response);
+            //console.log(response);
             $("#temp-display").html("");
             // Loops through the results array and places a marker for each
             for (var i = 0; i < 10; i++) {
-                console.log(response);
-                console.log(response.results[i].geometry.location);
+               // console.log(response);
+                //console.log(response.results[i].geometry.location);
                 var coords = response.results[i].geometry.location;
                 var name = response.results[i].name;
                 var rating = response.results[i].rating;
@@ -83,13 +83,13 @@ function getLocation() {
 function showPosition(position) {
     userPosition.lat = position.coords.latitude;
     userPosition.lng = position.coords.longitude;
-    console.log(userPosition);
+    //console.log(userPosition);
 }
 
 $(document).ready(function(){
 
 
-    getLocation();
-    initMap();
-    queryGMapsAPI(searchterm);
+//    getLocation();
+//    initMap();
+//    queryGMapsAPI(searchterm);
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 
     var cuisineArr = ["Asian", "Bubble Tea", "Cambodian", "Cantonese", "Chinese", "Dim Sum", "Filipino", "Fusion", "Japanese", "Korean", "Laotion", "Mongolian", "Nepalese", "Ramen", "Seafood", "Sushi", "Taiwanese", "Teriyaki", "Thai", "Vietnamese"]
+    var zomatorAPI="https://developers.zomato.com/api/v2.1/"
+    var zomatorKey="2764611985fca4aa535b451992f20776"
+    var start = 1
     var allRestArr = [];
 
     //Initialize carousel
@@ -17,9 +20,55 @@ $(document).ready(function(){
         },
     }
     function populateTable(restaurantInfo){
-       var tr=$("<tr><td>"+restaurantInfo.name+"</td><td>"+restaurantInfo.address+"</td><td>"+ restaurantInfo.cuisine+"</td><td>"+restaurantInfo.ratingNum +"</td><td>"+restaurantInfo.ratingUsers+"</td><td>"+restaurantInfo.ratingText+"</td></tr>")
+       var tr=$("<tr id='"+restaurantInfo.id+"'><td>"+restaurantInfo.name+"</td><td>"+restaurantInfo.address+"</td><td>"+ restaurantInfo.cuisine+"</td><td>"+restaurantInfo.ratingNum +"</td><td>"+restaurantInfo.ratingUsers+"</td><td>"+restaurantInfo.ratingText+"</td></tr>")
            $("#restaurantList").append(tr)
     }
+
+    function zomatorSearch(){
+        $.ajax({
+            "async"         :   true,
+            "crossDomain"   :   true,
+            "url"           :   zomatorAPI+ "search?entity_id=302&entity_type=city&q=92111&start="+start+"&count=5",
+            "method"        :   "GET",
+            "headers"       : {
+                "Accept"    : "application/json",
+                "user-key"  : zomatorKey
+            }
+        }).done(function(results){
+            traverseResults(results)
+        })
+    }
+
+    function traverseResults(results){
+        console.log("results",results.results_shown)
+
+        for (var i = 0; i< results.results_shown; i++){
+            var info={
+                "id"               : results.restaurants[i].restaurant.id,
+                "name"             : results.restaurants[i].restaurant.name,
+                "address"          : results.restaurants[i].restaurant.location.address,
+                "cuisine"          : results.restaurants[i].restaurant.cuisines,
+                "ratingNum"        : results.restaurants[i].restaurant.user_rating.aggregate_rating,
+                "restaurantInfo"   : results.restaurants[i].restaurant.user_rating.votes,
+                "ratingText"       : results.restaurants[i].restaurant.user_rating.rating_text
+            }
+            //if( cuisineArr.indexOf(info.cuisine) !== -1 ){
+                populateTable(info)
+
+            //}
+
+
+
+        }
+
+
+    }
+
+    $("#load-more").on("click",function(){
+        start= start +5
+        zomatorSearch();
+    })
+
 
 
     function callZomatoAPI() {
@@ -52,6 +101,8 @@ $(document).ready(function(){
 
         });
     }
-    callZomatoAPI();
+    //initial load
+    zomatorSearch()
+
 });
 
