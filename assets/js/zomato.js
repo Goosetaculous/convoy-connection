ac3fae1623f3aef2c826c3f3e09b560f327487c7
Shirlon Banks <sssbanks@gmail.com>
2017-06-07 17:53:54 -0700

    update

--- assets/js/zomato.js
+++ assets/js/zomato.js
@@ -55,7 +55,8 @@ $(document).ready(function(){
     function populateTable(restaurantInfo){
         var li = $("<li class='res-li' id='"+restaurantInfo.id+"'>" +
             "<div class='collapsible-header row' res-id='"+restaurantInfo.id+"'>" +
-            "<div class='col s4 getName' data-name='"+restaurantInfo.name+"'>"+restaurantInfo.name+"</div>" +
+            "<div class='col s3 getName' data-name='"+restaurantInfo.name+"'>"+restaurantInfo.name+"</div>" 
+            "<div class='col s3'>"+restaurantInfo.ratingNum+"</div>"
             "<div class='col s4'>"+restaurantInfo.address+"</div>" +
             "<div class='col s4'>"+ restaurantInfo.cuisine+"</div>" +
             "</div>" +
