a5f62e0425d841577bd6d15d9bf9da0af0f5c62e
Shirlon Banks <sssbanks@gmail.com>
2017-06-09 10:23:45 -0700

    update

--- assets/js/zomato.js
+++ assets/js/zomato.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
     var zomatorAPI="https://developers.zomato.com/api/v2.1/"
 
     //Backup key
-    //var zomatorKey = "ec761b592e1a11adbc7320c4fff471b9"
+    var zomatorKey = "ec761b592e1a11adbc7320c4fff471b9"
     
-    //active key as of 12p 6/8/17
-    var zomatorKey="02a56259c797204a75f7d4dd14a08d39"
+    //Maxed out @ 531p :Use on 6/9/17 after 531p
+    //var zomatorKey="02a56259c797204a75f7d4dd14a08d39"
     //Maxed out :Use on 6/9/17
     //var zomatorKey="2764611985fca4aa535b451992f20776"
 
-    var zomatorKey="02a56259c797204a75f7d4dd14a08d39"
-// >>>>>>> ce2d180ca928573a1d166273b5bc24d37f511b61
+    
+
     var zomatoAjax={
         "async"         :   true,
         "crossDomain"   :   true,
